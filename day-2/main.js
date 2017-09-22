53e0ff90ed6645051ead876ce6b0faea3930522d
Alex Close <ajclose@gmail.com>
2017-09-22 10:01:28 -0500

    Move hands of clock

--- day-2/main.js
+++ day-2/main.js
@@ -1,1 +1,18 @@
-console.log("hello!");
+const secondHand = document.querySelector(".second-hand")
+const minuteHand = document.querySelector(".minute-hand")
+const hourHand = document.querySelector(".hour-hand")
+
+function setDate() {
+  const now = new Date()
+  const seconds = now.getSeconds()
+  const secondsDegree = ((seconds/60) * 360) + 90
+  const minutes = now.getMinutes()
+  const minutesDegree = ((minutes/60) * 360) + 90
+  const hours = now.getHours()
+  const hoursDegree = ((hours/12) * 360) + 90
+  secondHand.style.transform = `rotate(${secondsDegree}deg)`
+  minuteHand.style.transform = `rotate(${minutesDegree}deg)`
+  hourHand.style.transform = `rotate(${hoursDegree}deg)`
+}
+
+setInterval(setDate, 1000)
