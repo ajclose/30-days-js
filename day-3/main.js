8e5f77c9053921c578596000edee37063648ce20
Alex Close <ajclose@gmail.com>
2017-09-23 09:25:01 -0500

    Change css variables with js

--- day-3/main.js
+++ day-3/main.js
@@ -1,1 +1,10 @@
-console.log("hello");
+const inputs = document.querySelectorAll(".controls input")
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || ""
+  const value = this.value
+  document.documentElement.style.setProperty(`--${this.name}`, value + suffix)
+}
+
+inputs.forEach(input => input.addEventListener("change", handleUpdate))
+inputs.forEach(input => input.addEventListener("mousemove", handleUpdate))
