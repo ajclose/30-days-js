9b031a615ecee192324356f6e622217b284ac756
Alex Close <ajclose@gmail.com>
2017-09-28 09:20:59 -0500

    Add styling to lines

--- day-8/main.js
+++ day-8/main.js
@@ -5,22 +5,35 @@ canvas.height = window.innerHeight
 context.strokeStyle = '#BADA55'
 context.lineJoin = 'round'
 context.lineCap = 'round'
+context.lineWidth = 50
 
 let isDrawing = false
 let lastX = 0
 let lastY = 0
+let hue = 0
+let direction = true
 
 function draw(e) {
   if (!isDrawing) {
     return
   }
   console.log(e);
+  context.strokeStyle = `hsl(${hue}, 100%, 50%)`
   context.beginPath()
   context.moveTo(lastX, lastY)
   context.lineTo(e.offsetX, e.offsetY)
   context.stroke()
   lastX = e.offsetX
   lastY = e.offsetY
+  hue++
+  if (context.lineWidth >= 100 || context.lineWidth <= 1) {
+    direction = !direction
+  }
+  if (direction) {
+    context.lineWidth++
+  } else {
+    context.lineWidth--
+  }
 }
 
 canvas.addEventListener("mousemove", draw)
