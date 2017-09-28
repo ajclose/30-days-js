b4ffd43934d2ed8aa5ee3677a186ca8501518a15
Alex Close <ajclose@gmail.com>
2017-09-28 09:10:09 -0500

    Can draw on page

--- day-8/main.js
+++ day-8/main.js
@@ -1,1 +1,37 @@
+const canvas = document.querySelector("canvas")
+const context = canvas.getContext('2d')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+context.strokeStyle = '#BADA55'
+context.lineJoin = 'round'
+context.lineCap = 'round'
 
+let isDrawing = false
+let lastX = 0
+let lastY = 0
+
+function draw(e) {
+  if (!isDrawing) {
+    return
+  }
+  console.log(e);
+  context.beginPath()
+  context.moveTo(lastX, lastY)
+  context.lineTo(e.offsetX, e.offsetY)
+  context.stroke()
+  lastX = e.offsetX
+  lastY = e.offsetY
+}
+
+canvas.addEventListener("mousemove", draw)
+canvas.addEventListener("mousedown", function(e) {
+  isDrawing = true
+  lastX = e.offsetX
+  lastY = e.offsetY
+})
+canvas.addEventListener("mouseup", function(e) {
+  isDrawing = false
+})
+canvas.addEventListener("mouseout", function(e) {
+  isDrawing = false
+})
