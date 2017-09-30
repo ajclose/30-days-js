d673f6c1c13627c8092a6da83f773669b583b7dd
Alex Close <ajclose@gmail.com>
2017-09-30 15:54:19 -0500

    Hold shift to select all items in between

--- day-10/main.js
+++ day-10/main.js
@@ -1,1 +1,25 @@
-console.log("hello!");
+const checkboxes = document.querySelectorAll("input[type='checkbox']")
+let lastItem
+
+function handleCheck(e) {
+  let inBetween = false
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach(checkbox => {
+      if (checkbox === this || checkbox === lastItem) {
+        inBetween = !inBetween
+      }
+
+      if (inBetween) {
+        checkbox.checked = true
+      }
+    })
+  }
+
+  lastItem = this
+}
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck))
+document.addEventListener("keydown", function(e) {
+  if (e.key != "Shift") {
+    return
+  }
+})
