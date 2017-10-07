5e943e2778055099214a20b10595e3754c3e7f4b
Alex Close <ajclose@gmail.com>
2017-10-07 08:58:04 -0500

    Create function to sort ignoring articles a, an, the

--- day-17/main.js
+++ day-17/main.js
@@ -1,2 +1,13 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-console.log("hello");
+const bandList = document.querySelector("#bands")
+
+function strip(bandName) {
+  return bandName.replace(/^(a |an |the )/i, "").trim()
+}
+
+const sortedBands = bands.sort((a,b) => strip(a) > strip(b) ? 1 : -1)
+
+sortedBands.forEach(band => {
+  const html = `<li>${band}</li>`
+  bandList.insertAdjacentHTML("beforeend", html)
+})
