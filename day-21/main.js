3b333f323a76518e804c17e0f00914b5297c828d
Alex Close <ajclose@gmail.com>
2017-10-11 14:32:17 -0500

    Display speed and direction of user

--- day-21/main.js
+++ day-21/main.js
@@ -1,1 +1,9 @@
-console.log("hello")
+const arrow = document.querySelector('.arrow')
+const speed = document.querySelector('.speed-value')
+
+navigator.geolocation.watchPosition((data) => {
+  speed.textContent = data.coords.speed
+  arrow.style.transform = `rotate(${data.coords.heading}deg)`
+}, err => {
+  console.error(err);
+})
