13c023f725c1ae7c96298e92ee800524047ef777
Alex Close <ajclose@gmail.com>
2017-10-12 08:53:33 -0500

    Create link highlighting that follows mouse

--- day-22/main.js
+++ day-22/main.js
@@ -1,1 +1,21 @@
-console.log("hello");
+const links = document.querySelectorAll('a')
+const highlight = document.createElement('span')
+highlight.classList.add('highlight')
+document.body.append(highlight)
+
+function highlightLink() {
+  const linkCoords = this.getBoundingClientRect()
+  console.log(linkCoords);
+  const coords = {
+    height: linkCoords.height,
+    width: linkCoords.width,
+    top: linkCoords.top + scrollY,
+    left: linkCoords.left + scrollX
+  }
+  highlight.style.height = `${coords.height}px`
+  highlight.style.width = `${coords.width}px`
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`
+
+}
+
+links.forEach(link => link.addEventListener('mouseenter', highlightLink))
