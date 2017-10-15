5453be43688333a208ceca848dc77eb68d3dd8f3
Alex Close <ajclose@gmail.com>
2017-10-15 09:50:08 -0500

    Practice with propagation, bubbling, and once

--- day-25/main.js
+++ day-25/main.js
@@ -1,1 +1,18 @@
-console.log("hello");
+const divs = document.querySelectorAll('div')
+const button = document.querySelector('button')
+
+function logText(e) {
+  console.log(this.classList.value);
+  // e.stopPropagation()
+}
+
+divs.forEach(div => div.addEventListener('click', logText, {
+  capture: false,
+  once: true
+}))
+
+button.addEventListener('click', () => {
+  console.log("click!!");
+}, {
+  once: true
+})
