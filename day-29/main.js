582903a6148f3133a6b06495415a2a99f783c58e
Alex Close <ajclose@gmail.com>
2017-10-19 08:42:57 -0500

    Display countdown on page

--- day-29/main.js
+++ day-29/main.js
@@ -1,1 +1,23 @@
-console.log("hello");
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left')
+
+function timer(seconds) {
+  const now = Date.now()
+  const then = now + seconds * 1000
+  displayTimeLeft(seconds)
+countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000)
+    if (secondsLeft < 0) {
+      clearInterval(countdown)
+      return
+    }
+    displayTimeLeft(secondsLeft)
+  }, 1000)
+}
+
+function displayTimeLeft(seconds) {
+  const minutes = Math.floor(seconds / 60)
+  const secs = seconds % 60
+  const display = `${minutes}:${secs < 10 ? '0' : ''}${secs}`
+  timerDisplay.textContent = display
+}
