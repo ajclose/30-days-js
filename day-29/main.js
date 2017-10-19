315dbf746ed5425af0782d658b296f837cd4c293
Alex Close <ajclose@gmail.com>
2017-10-19 08:58:31 -0500

    Hook up buttons to timer

--- day-29/main.js
+++ day-29/main.js
@@ -1,8 +1,10 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left')
 const endDisplay = document.querySelector('.display__end-time')
+const buttons = document.querySelectorAll('[data-time]')
 
 function timer(seconds) {
+  clearInterval(countdown)
   const now = Date.now()
   const then = now + seconds * 1000
   displayTimeLeft(seconds)
@@ -32,3 +34,10 @@ function displayEndTime(timestamp) {
   const display = `Be Back At ${hour > 12 ? hour - 12 : hour}:${minutes < 10 ? '0' : ''}${minutes}`
   endDisplay.textContent = display
 }
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time)
+  timer(seconds)
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer))
